b7f6aefbe3748b72cdb344e0acd8220df37e3463
epoxy99 <abill.abidin@gmail.com>
2024-08-09 14:21:32 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/tugas_modul_widget/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tugas_modul_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+       appBar:AppBar (
+         title: Text('Simple Calculator'),
+         titleTextStyle: TextStyle(
+          color: Colors.amber,
+          fontFamily: 'Monospace',
+          fontStyle: 
+         ),
+         backgroundColor:Colors.black
+       ),
+
+        
+        ),
+      );
+  }
+}
