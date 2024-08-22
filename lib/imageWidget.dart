8c2cef7d190a54555f2bbee722274fba1fa6fe9e
epoxy99 <abill.abidin@gmail.com>
2024-08-22 20:13:27 +0700

    scaffold_widget

--- lib/imageWidget.dart
+++ lib/imageWidget.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ImageWidget extends StatelessWidget {
   const ImageWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return 
+    Row(
       children: [
-        Image(image: AssetImage('assets/images/download.jpeg'),)
+        Image(image: AssetImage('assets/images/download.jpeg'),),
+      
         
       ],
 
--- lib/scaffold_widget.dart
+++ lib/scaffold_widget.dart
@@ -2,25 +2,67 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tugas_modul_widget/imageWidget.dart';
 
-class ScaffoldWidget extends StatelessWidget {
+class ScaffoldWidget extends StatefulWidget {
+ 
   const ScaffoldWidget({super.key});
 
   @override
+  State<ScaffoldWidget> createState() => _ScaffoldWidgetState();
+}
+
+class _ScaffoldWidgetState extends State<ScaffoldWidget> {
+   var _selectedIndex = 0;
+   onTapBar(index){
+    setState(() {
+      _selectedIndex = index;
+    });
+
+   }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('data'),
-        leading: IconButton(onPressed: (){},icon: Icon(Icons.radio_button_checked),),
-        backgroundColor: Colors.blue,
-        foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-        actions: [
-          SizedBox(width: 10,),
-          Icon(Icons.more_vert),
-          SizedBox(width: 10,),
-          Icon(Icons.logout)
-        ],
-      ),
-      body: ImageWidget(),
-    );
+        appBar: AppBar(
+          title: Text('Welcome Home'),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          backgroundColor: Colors.blue,
+          foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+          actions: [
+            SizedBox(
+              width: 10,
+            ),
+            Icon(Icons.more_vert),
+            SizedBox(
+              width: 10,
+            ),
+            Icon(Icons.logout)
+          ],
+        ),
+        body: ImageWidget(),
+        bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: Colors.blue,
+            unselectedItemColor: const Color.fromARGB(255, 150, 147, 147),
+            currentIndex: _selectedIndex,
+            onTap: onTapBar,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search",),
+                BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: "Search",)
+            ]));
   }
 }
